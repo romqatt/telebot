ae90ff02873faac8977ef4e39fefef09148e624f
romqatt <romqatt@gmail.com>
2024-01-22 22:22:53 +0300

    Add echo

--- bot.py
+++ bot.py
@@ -1,14 +1,8 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import ForceReply, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler, MessageHandler, filters
 
 import logging
 import settings
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    filename='bot.log',
-    level=logging.INFO
-)
 
 # Enable logging
 logging.basicConfig(
@@ -36,7 +30,7 @@ def main_menu() -> [list]:
     return keyboard
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    logger.info("User %s started the conversation.", update.message.from_user)
+    logging.info(update)
 
     keyboard = main_menu()
 
@@ -51,6 +45,11 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
     await update.message.reply_text(text, reply_to_message_id=update.effective_message.message_id)
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logging.info(update)
+
+    await update.message.reply_text(update.message.text, reply_to_message_id=update.effective_message.message_id)
+
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     logging.info(update)
 
@@ -64,12 +63,11 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         chat_id = update.effective_chat.id,
         text = f"Selected option: {query.data}"
     )
-    
+
 def start_bot():
     application = ApplicationBuilder().token(settings.bot_token).build()
-    
-    #echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
 
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     application.add_handler(CommandHandler('start', start_command))
     application.add_handler(CommandHandler('help', help_command))
     application.add_handler(CallbackQueryHandler(button))
